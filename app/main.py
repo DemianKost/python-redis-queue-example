2ebb5ec294ea9d8b9c2da3fe1d88ca55e6cbd5af
DemianKost <demiankost2@gmail.com>
2024-03-22 03:59:12 +0100

    Set up main.py for the main application

--- app/main.py
+++ app/main.py
@@ -23,3 +23,42 @@ def redis_db():
     db.ping()
 
     return db
+
+def redis_queue_push(db, message):
+    # push to tail of the queue
+    db.lpush(config.redis_queue_name, message)
+
+def main(num_messages: int, delay: float = 1):
+    """
+    Generate num of messages and push it to the Redis queue
+    :param num_messages:
+    :return:
+    """
+
+    # Connect to Redis db
+    db = redis_db()
+
+    for i in range(num_messages):
+        # Setup message data
+        message = {
+            "id": str(uuid4()),
+            "ts": datetime.utcnow().isoformat(),
+            "data": {
+                "message_number": i,
+                "x": random.randrange(0, 100),
+                "y": random.randrange(0, 100),
+            }
+        }
+
+        # All message data we will store as JSON in Redis DB
+        message_json = dumps(message)
+
+        # Push message to queue
+        print(f"Sending message {i} (id={message['id']})")
+        redis_queue_push(db, message_json)
+
+        # Wait a little bit
+        sleep(delay)
+
+if __name__ == "__main__":
+    main(30, 0.1)
